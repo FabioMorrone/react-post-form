3f71c1fefed23ed10c8636dd8ba3181892e6880f
FabioMorrone <morronefabio04@gmail.com>
2025-03-31 15:36:01 +0200

    enhance form structure in App component and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,44 @@ function App() {
     public: '',
   })
 
+
+
+
   return (
     <>
-      <div className='container'>
+      <div className='container p-5'>
 
+        <form className="form-label mb-3">
+          <input
+            type="text"
+            name="author"
+            class="form-control mb-2"
+            placeholder="author"
 
-        <form className='form'></form>
+          />
+          <input
+            type="text"
+            name="title"
+            class="form-control mb-2"
+            placeholder="title"
+
+          />
+          <input
+            type="text"
+            name="body"
+            class="form-control mb-2"
+            placeholder="body"
+
+          />
+          <input
+            type="text"
+            name="public"
+            class="form-control mb-2"
+            placeholder="public"
+
+          />
+          <button type="submit" className="btn btn-secondary mt-5">Submit</button>
+        </form>
 
       </div>
 
